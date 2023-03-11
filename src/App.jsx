23682b1d80736d893a7188c5f2f348842db27cc1
Dyeizon <dyeizonque@gmail.com>
2023-03-11 14:57:34 -0300

    start of major arcana data

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Services } from "./components/Services";
 import { Menu } from "./components/Menu";
 import { Footer } from "./components/Footer";
 import { ContextProvider } from './contexts/MenuContext';
+import { Route, BrowserRouter } from 'react-router-dom';
 
 export const App = () => {
   return (
---
+++ src/assets/data/majorarcana.js
@@ -0,0 +1,18 @@
+export default majorarcana = [
+    {
+        "number": 0,
+        "name": "O Louco",
+        "desc": "O loco"
+    },
+    
+    {
+        "number": 1,
+        "name": "O Mago",
+        "desc": "O mago"
+    },
+    {
+        "number": 2,
+        "name": "A Imperatriz",
+        "desc": "A imperatrizzz"
+    },
+]
