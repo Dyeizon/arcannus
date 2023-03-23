71e67a310445018812f594ce5dd254f36f2d0f0b
Dyeizon <dyeizonque@gmail.com>
2023-03-23 19:56:30 -0300

    Major Arcana description changed

--- src/assets/data/majorarcana.js
+++ src/assets/data/majorarcana.js
@@ -2,154 +2,154 @@ export const majorarcana = [
     {
         "number": 0,
         "name": "O Louco",
-        "desc": "Simboliza novos começos, novas experiências, simboliza o início da jornada. A carta sugere que esse novo começo seja empreendido de braços abertos e com alegria.",
+        "desc": "Significa a sua vida é cheia de riscos, recomeços, e momentos de evolução. O Louco aventura-se, arrisca-se, se lança no abismo e enfrenta as novas situações.",
         "img": "https://www.astrolink.com.br/data/tarot/o-louco.jpg"
     },
     
     {
         "number": 1,
         "name": "O Mago",
-        "desc": "Traz a mensagem de que seus talentos e seus próprios dons são as chaves para o seu sucesso em qualquer situação questionada. É como se você tivesse todas as ferramentas que precisa bem na sua frente para garantir que o sucesso potencial se manifeste.",
+        "desc": "Significa que você tem um poder de iniciativa, grande habilidade e influência, capta o poder mágico que vem do alto para o nosso plano, o plano material.",
         "img": "https://www.astrolink.com.br/data/tarot/o-mago.jpg"
     },
 
     {
         "number": 2,
         "name": "A Sacerdotisa",
-        "desc": "É uma figura aparentemente perfeita, mas sugere possuir mais segredos do que aparenta. Na verdade, este Arcano Maior está de fato envolto a mistérios.",
+        "desc": "Significa que você tem muita paciência, grande poder de intuição e compreensão do mundo e da humanidade.",
         "img": "https://www.astrolink.com.br/data/tarot/a-sacerdotisa.jpg"
     },
 
     {
         "number": 3,
         "name": "A Imperatriz",
-        "desc": "A Imperatriz é a eterna mãe do Tarot. Sentada em seu trono com seu cetro dourado, tudo ao seu redor parece exalar beleza, sucesso e riqueza. Sua coroa brilha com as 12 estrelas do zodíaco e ela está cercada por lindos jardins.",
+        "desc": "Você é uma pessoa em crescimento, com muita vivacidade e que vai viver a vida em constante modificação.",
         "img": "https://www.astrolink.com.br/data/tarot/a-imperatriz.jpg"
     },
 
     {
         "number": 4,
         "name": "O Imperador",
-        "desc": "Representa a ordem e a estrutura. Simboliza a energia masculina e basicamente gira em torno de poder e a autoridade.",
+        "desc": "Representa que você tem grande poder de estabilidade, apreço pela ordem e pela segurança em sua vida.",
         "img": "https://www.astrolink.com.br/data/tarot/o-imperador.jpg"
     },
 
     {
         "number": 5,
         "name": "O Papa",
-        "desc": "É o nosso guia, aquele que pega pela mão e nos leva ao nosso objetivo espiritual. Ele representa um caminho para a consciência superior.",
+        "desc": "Significa que você tem uma aguçada percepção intuitiva e grande poder de organização.",
         "img": "https://www.astrolink.com.br/data/tarot/o-papa.jpg"
     },
 
     {
         "number": 6,
         "name": "Os Amantes",
-        "desc": "Este Arcano diz para seguir o coração na hora de tomar alguma decisão importante, mas sem esquecer de ponderar sobre as consequências, sugerindo que deva existir muita reflexão envolvida.",
+        "desc": "Representa que você tem uma tendência à dúvida, à indecisão, a viver momentos de importantes escolhas. É símbolo também do livre arbítrio.",
         "img": "https://www.astrolink.com.br/data/tarot/os-enamorados.jpg"
     },
 
     {
         "number": 7,
         "name": "A Carruagem",
-        "desc": "Seu simbolismo sugere que você deve ser o(a) dono(a) do seu destino e tomar as rédeas da situação rumo à vitória.",
+        "desc": "Significa que você é uma pessoa de força e determinação e que é o momento de partir com tudo para novos projetos.",
         "img": "https://www.astrolink.com.br/data/tarot/o-carro.jpg"
     },
 
     {
         "number": 8,
         "name": "A Força",
-        "desc": "A Força representa as melhores características do signo de Leão, como coragem, determinação, resiliência e força, boas chaves para o sucesso.",
+        "desc": "Significa que você terá sucesso, vitalidade e domínio em sua vida.",
         "img": "https://www.astrolink.com.br/data/tarot/a-forca.jpg"
     },
 
     {
         "number": 9,
         "name": "O Eremita",
-        "desc": "Indica que não é hora de agir impulsivamente e sim de passar um tempo pensando nas coisas que estão acontecendo.",
+        "desc": "Significa a introversão, a necessidade de autodescoberta, de atingir a maturidade e a sabedoria. Pode significar também que você precisa silenciar sobre as coisas que pretender fazer, não ficar falando os seus planos aos quatro ventos.",
         "img": "https://www.astrolink.com.br/data/tarot/o-eremita.jpg"
     },
 
     {
         "number": 10,
         "name": "A Roda da Fortuna",
-        "desc": "Representa basicamente a colheita daquilo que plantamos. Apesar do seu significado otimista, não sugere prosperidade, sorte e fortuna do nada.",
+        "desc": "Significa que a sua vida é repleta de mudanças inesperadas, da possibilidade de vencimento de um karma ou da alternância da sorte/azar.",
         "img": "https://www.astrolink.com.br/data/tarot/a-roda-da-fortuna.jpg" 
     },
 
     {
         "number": 11,
         "name": "A Justiça",
-        "desc": "Libra é o signo associado a este Arcano, signo que sugere igualdade, honestidade, harmonia e equilíbrio, qualidades advindas da balança da justiça.",
+        "desc": "Significa que você tem grande poder de decisão em suas mãos, é alguém que vai colher o que plantou.",
         "img": "https://www.astrolink.com.br/data/tarot/a-justica.jpg"
     },
 
     {
         "number": 12,
         "name": "O Enforcado",
-        "desc": "O Enforcado o aconselha a deixar as coisas como estão por um momento e olhar para a sua situação de uma perspectiva diferente. Enquanto você está em um momento de suspensão, você pode reconhecer pontos de vista que lhe dirão qual deve ser seu próximo movimento.",
+        "desc": "Significa que você pode ter uma realidade difícil, crise, resignação que será preciso lutar contra ou ela irá tomar conta de toda a vida.",
         "img": "https://www.astrolink.com.br/data/tarot/o-enforcado.jpg"
     },
 
     {
         "number": 13,
         "name": "A Morte",
-        "desc": "Pode incutir medo ou receio nos que ainda não compreendem seu significado. Algo pode estar prestes a mudar e possivelmente trará renascimento, melhorando a condição da situação em questão. ",
+        "desc": "Significa o fim de uma situação difícil, e ao lado das cabeças há plantas nascendo, essas plantas mostram que é necessário que algo seja feito para que haja um renascimento, um reinício.",
         "img": "https://www.astrolink.com.br/data/tarot/a-morte.jpg"
     },
 
     {
         "number": 14,
         "name": "A Temperança",
-        "desc": "Significa equilíbrio e sugere harmonia para as situações em que se envolve. É uma carta que indica mais pensamento do que ação. Você não precisa gastar um monte de energia para resolver sua situação, muito pelo contrário.",
+        "desc": "Significa que a paciência é uma virtude para atingir o equilíbrio e a realização dos seus desejos. Será preciso paz e conciliação.",
         "img": "https://www.astrolink.com.br/data/tarot/a-temperanca.jpg"
     },
 
     {
         "number": 15,
         "name": "O Diabo",
-        "desc": "Sua associação com Capricórnio sugere que você deve tomar o controle da situação em questão, lidando com as coisas de maneira bem pragmática.",
+        "desc": "Nesta carta, um diabo alado aparece prendendo duas pessoas, uma em cada mão, com correntes. Ter tirado esse Arcano Pessoal pode indicar excesso de ambição, de desejo e de ilusão em sua vida.",
         "img": "https://www.astrolink.com.br/data/tarot/o-diabo.jpg"
     },
 
     {
         "number": 16,
         "name": "A Torre",
-        "desc": "O significado da carta tem a ver com mudanças abruptas. Algo pode mudar em relação às situações em que se envolve. Pense em uma estrutura ou fundação que entra em colapso.",
+        "desc": "Tirar essa carta como o seu arcano pessoal pode alertar para uma transformação em sua vida, o fim (possivelmente desastroso) de uma situação, um prejuízo, algo não agradável.",
         "img": "https://www.astrolink.com.br/data/tarot/a-torre.jpg"
     },
 
     {
         "number": 17,
         "name": "A Estrela",
-        "desc": "Indica um momento em que as coisas começam a dar certo. Sugere que conflitos podem ser apaziguados, problemas de saúde curados, dívidas quitadas, separações que viram reuniões e situações que se tornam positivas.",
+        "desc": "Tirar essa carta pode significar que depois dos desastres e das desgraças, a vida sempre nasce com esperança. Sorte, otimismo e realização se aproximam de você.",
         "img": "https://www.astrolink.com.br/data/tarot/a-estrela.jpg"
     },
 
     {
         "number": 18,
         "name": "A Lua",
-        "desc": "Significa que está na hora de você seguir a sua intuição. Indica segredos, mistérios e ilusões. Se você tem que tomar uma decisão e não tem certeza sobre que caminho tomar, escute seu coração.",
+        "desc": "Ser representado por esse arcano maior pode indicar a presença de uma depressão, tristeza, ansiedade.",
         "img": "https://www.astrolink.com.br/data/tarot/a-lua.jpg"
     },
 
     {
         "number": 19,
         "name": "O Sol",
-        "desc": "Simboliza basicamente luz, iluminação. Geralmente indica um momento extremamente positivo, pois a carta é uma das mais alegres e otimistas do Tarot.",
+        "desc": "Tirar essa carta como arcano significa alegria, prosperidade e sorte em sua vida.",
         "img": "https://www.astrolink.com.br/data/tarot/o-sol.jpg"
     },
 
     {
         "number": 20,
         "name": "O Julgamento",
-        "desc": "Significa que um novo ciclo se inicia. Todo o seu desempenho ou os pormenores da questão serão avaliados e recompensas surgirão.",
+        "desc": "Essa carta pode representar um desejo de renovação, de transcendência, de ouvir novos chamados e viver novidades.",
         "img": "https://www.astrolink.com.br/data/tarot/o-julgamento.jpg"
     },
 
     {
         "number": 21,
         "name": "O Mundo",
-        "desc": " Significa que a situação em questão terá um desfecho. Anuncia um novo começo e indica que a mudança é inevitável. As mudanças que ocorrem ou ocorrerão tendem a ser positivas, sugerindo resposta positiva.",
+        "desc": "Ter essa carta como seu arcano pessoal pode significar a perfeição, a busca pelo seu melhor momento, é o ápice, a melhor carta do Tarot.",
         "img": "https://www.astrolink.com.br/data/tarot/o-mundo.jpg"
     },
 ]
